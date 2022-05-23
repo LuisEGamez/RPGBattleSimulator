9314d6e941e6286967b2a5f705986d50741557be
Rocio <roci.ramos87@gmail.com>
2022-05-23 16:02:06 +0200

    test commit

--- src/classes/Warrior.java
+++ src/classes/Warrior.java
@@ -1,11 +1,13 @@
 package classes;
 
+import java.math.BigDecimal;
+
 public class Warrior extends Character {
     private int stamina;
-    private int strength;
+    private double strength;
 
-    public Warrior(int id, String name, int hp, boolean isAlive, int stamina, int strength) {
-        super(id, name, hp, isAlive);
+    public Warrior(int id, String name, BigDecimal hp, boolean isAlive, BigDecimal manage, int stamina, double strength) {
+        super(id, name, hp, isAlive, manage);
         setStamina(stamina);
         setStrength(strength);
     }
@@ -18,16 +20,28 @@ public class Warrior extends Character {
         this.stamina = stamina;
     }
 
-    public int getStrength() {
+    public double getStrength() {
         return strength;
     }
 
-    public void setStrength(int strength) {
+    public void setStrength(double strength) {
         this.strength = strength;
     }
 
     @Override public void attack() {
-        //if possible HEAVY ATTACK
-        // else WEAK ATTACK
+        if(stamina >= 5){
+            System.out.println("I'm going to make a Heavy attack");
+            setDamage(BigDecimal.valueOf(strength));
+            decreaseHP();
+            stamina -= 5;
+        } else {
+            System.out.println("I'm going to make a Weak attack");
+            setDamage(BigDecimal.valueOf((strength / 2)));
+            decreaseHP();
+            stamina += 1;
+        }
+
     }
+
+
 }
